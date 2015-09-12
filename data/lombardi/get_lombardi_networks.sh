9cd3f0da2d18c566e363251d97d87aad5723c44b
Hendrik Bergunde <hbergunde@gmx.de>
2015-09-12 09:30:27 +0200

    Delete duplicate nodes upon downloading Lombardi nets

--- data/lombardi/get_lombardi_networks.sh
+++ data/lombardi/get_lombardi_networks.sh
@@ -2,3 +2,7 @@
 urls=$(curl http://www.lombardinetworks.net/networks/the-networks/ | grep -i -o 'http://www.lombardi[^>]\+\.graphml' | sort | uniq)
 
 wget --no-directories -N $urls
+
+# these files contain some duplicate line errors, delete them:
+sed -e'776,779d' -i HarkenBush5th.graphml
+sed -e'948,951d' -i NuganHandBank.graphml
